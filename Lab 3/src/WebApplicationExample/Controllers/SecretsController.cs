989ea37c1e1c6df94c81fd03c2fc7b47a47998d0
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-10-18 15:08:14 +0200

    SecretsController

---
+++ Lab 3/src/WebApplicationExample/Controllers/SecretsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplicationExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecretsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SecretsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return $"Host: {Environment.MachineName}\n" + $"Secret value: {_configuration["MySecret"]}";
+        }
+    }
+}
--- Lab 3/src/WebApplicationExample/Program.cs
+++ Lab 3/src/WebApplicationExample/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace WebApplicationExample
 {
@@ -12,6 +14,11 @@ namespace WebApplicationExample
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.SetBasePath(Directory.GetCurrentDirectory());
+                    config.AddJsonFile("secrets/appsettings.secrets.json", optional: true, reloadOnChange: true);
+                })
                 .UseStartup<Startup>();
     }
 }
